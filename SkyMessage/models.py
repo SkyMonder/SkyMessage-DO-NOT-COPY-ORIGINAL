64ebc5161746c56d803887690913feef670da1ef
SkyMonder <rilloperdagelo@gmail.com>
2025-09-02 21:13:37 +0300

    Add files via upload

---
+++ SkyMessage/models.py
@@ -0,0 +1,20 @@
+from extensions import db
+from sqlalchemy import UniqueConstraint
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
+
+class Chat(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user1_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user2_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    __table_args__ = (UniqueConstraint('user1_id','user2_id', name='uq_chat_users'),)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    chat_id = db.Column(db.Integer, db.ForeignKey('chat.id'), index=True, nullable=False)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, default=db.func.now(), index=True)
