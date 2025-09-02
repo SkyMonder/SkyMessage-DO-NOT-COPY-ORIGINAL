de52b89048d576553a2af1269adf7ccab6c08563
SkyMonder <rilloperdagelo@gmail.com>
2025-09-02 22:09:59 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,20 +1,19 @@
+from datetime import datetime
 from extensions import db
-from sqlalchemy import UniqueConstraint
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(200), nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
 
 class Chat(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user1_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     user2_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    __table_args__ = (UniqueConstraint('user1_id','user2_id', name='uq_chat_users'),)
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    chat_id = db.Column(db.Integer, db.ForeignKey('chat.id'), index=True, nullable=False)
+    chat_id = db.Column(db.Integer, db.ForeignKey('chat.id'), nullable=False)
     sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     text = db.Column(db.Text, nullable=False)
-    timestamp = db.Column(db.DateTime, default=db.func.now(), index=True)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
