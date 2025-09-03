8fdd83c80675dc60759a159d73268bfada56550d
SkyMonder <rilloperdagelo@gmail.com>
2025-09-03 21:48:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -127,4 +127,4 @@ def handle_answer(data):
 # --- Run ---
 if __name__ == '__main__':
     with app.app_context(): db.create_all()
-    socketio.run(app, host='0.0.0.0', port=int(os.environ.get("PORT",5000)))
+   socketio.run(app, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), allow_unsafe_werkzeug=True)
